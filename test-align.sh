444a7eab73c43095b19b46662e001e1dbd65a99e
Jeff Kaufman <jeff@jefftk.com>
2025-03-20 10:21:22 -0400

    align: add tests for additional features

--- test-align.sh
+++ test-align.sh
@@ -5,8 +5,14 @@ source test-util.sh
 
 check align-all              tests/a.fasta tests/a.fasta
 check align-specific         tests/a.fasta:seq123 tests/a.fasta:seq456
+check align-specific-num     tests/a.fasta:0 tests/a.fasta:1
 check align-fail             tests/a.fasta:seq123 tests/a.fasta:seq789
 check min-score              tests/a.fasta:seq123 tests/a.fasta:seq789 \
                                 --min-score 0
 check align-fastq            tests/b.fastq tests/b.fastq
 check literal                tests/b.fastq:fq2 ACAACCTACCGTGACAAAGAAAGTTGTC
+check literal-rc             GACAACTTTCTTTTTGTCACGGTAGGTTGT \
+                             rc:ACAACCTACCGTGACAAAGAAAGTTGTC
+check print-progress         tests/b.fastq tests/b.fastq --print-progress
+check copyable-alignment     tests/b.fastq tests/b.fastq \
+                             --just-print-copyable-alignment
