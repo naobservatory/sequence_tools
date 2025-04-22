83fef1f83bf4816d2e9932c5d7ce8739c00f785d
Jeff Kaufman <jeff@jefftk.com>
2025-04-22 10:08:43 -0400

    document test framework

--- test-util.sh
+++ test-util.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 
+# Test framework.
+#
+# Each test should look like either:
+#
+#    check ...
+#
+# Or:
+#
+#    check_pipeline ...
+#
+# In either case, the framework will verify that running the command produces
+# the expected "gold" output.  To update the gold outputs to match current
+# behavior, run with --regold.
+#
+# By default runs all the tests, or you can specify the name of a specific test
+# to run.
+
 if [ "$#" -gt 0 -a "$1" = "--regold" ]; then
   REGOLD="true"
   shift
@@ -23,12 +40,20 @@ function fail() {
   exit 1
 }
 
+# Usage: check test-name arg1 arg2 arg3
+#
+# Verify that the command we're testing (stored in global variable INVOCATION
+# along with standard arguments) produces the expected output when run on the
+# arguments.
 function check() {
   local this_test="$1"
   shift
   check_pipeline "$this_test" "$INVOCATION $@"
 }
 
+# Usage: check-pipeline test-name 'foo | bar | baz'
+#
+# Verify that the pipeline we're testing (argument 2) produces the expected output.
 function check_pipeline() {
   local error_code
   local this_test="$1"
